75a12ddbee6410981d5b0c581ed9e404292e7d07
ravinainn <nain.ravi12345@gmail.com>
2023-10-14 16:34:11 +0530

    fdsaas

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
 import express, { urlencoded } from "express";
 import cp from "cookie-parser";
 import { connection } from "./utils/connection.js";
-import cors from "cors"
+import cors from "cors";
 import router from "../routers/authrouter.js";
 import tutorrouter from "../routers/tutorPost.js";
 import dotenv from "dotenv";
 import studentrouter from "../routers/tutorPost.js";
-const app=express();
+const app = express();
 dotenv.config();
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 connection();
 app.use(cp());
-app.use(cors({
+app.use(
+  cors({
     // Access-Control-Allow-Origin:*,
-    origin:"http://localhost:3001",//upated
-    methods:["GET","PUT","POST","DELETE"],
-    credentials:true,//to make cookie accessible on frontend also bcz this allows cokkie accessible on diff domains.
-}));
-app.get("/",(req,res)=>{
-    res.send("hello");
-})
-app.use("/api/tutor-posts",tutorrouter);
-app.use("/api/student-posts",studentrouter);
-app.use(router)
-app.listen(3000,()=>{
-    console.log("server running on port 3000");
-})
+    origin: "*", //upated
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true, //to make cookie accessible on frontend also bcz this allows cokkie accessible on diff domains.
+  })
+);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use("/api/tutor-posts", tutorrouter);
+app.use("/api/student-posts", studentrouter);
+app.use(router);
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
