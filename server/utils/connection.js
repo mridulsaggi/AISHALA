138ba783a8b5a32d52dcf55387945d5322090502
ravinainn <nain.ravi12345@gmail.com>
2023-11-18 07:56:03 +0530

    did some changes in the tutorpost db

--- server/utils/connection.js
+++ server/utils/connection.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-export const connection=(req,res)=>{
-    
-   mongoose.connect(process.env.mongourl,{dbName:"aishala"}).then(()=>{
-        console.log("db connected");
-    }).catch((err)=>{
-        console.log("f")
-        console.log(err);
+export const connection = (req, res) => {
+  mongoose
+    .connect(process.env.Mongourl, { dbName: "aishala" })
+    .then(() => {
+      console.log("db connected");
+    })
+    .catch((err) => {
+      console.log("f");
+      console.log(err);
     });
-}
+};
